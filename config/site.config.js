a4c6c27e0504d6e649d3af8e8c5f844f3aa922ff
Yusima10xx <142825716+Yusima10xx@users.noreply.github.com>
2023-08-23 00:18:11 +0700

    site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -46,14 +46,14 @@ module.exports = {
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
-  telegram:  '',
+  
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
     {
-      name: '',
+      name: 'Telegram',
       link: '',
     },
   ],
